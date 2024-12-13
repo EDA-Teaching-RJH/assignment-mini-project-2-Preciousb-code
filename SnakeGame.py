a5d13336e6e04c9f8a23aaddfc5baa60194b3830
Preciousb-code <pb531@kent.ac.uk>
2024-12-13 08:28:25 +0000

    2

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,25 +1,19 @@
-import random
 import curses
 
-s = curses.initdcr()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
-w.keypad(1)
-w.timeout(100)
+# setup window
+curses.initscr() 
+win = curses.newwin(20,60,0,0)
+win.keypad(1) # arrow keys to control snake
+curses.noecho()
+curses.cur_set(0) 
+win.border(0)
+win.nodelay(1)#
 
-snk_x = sw/4
-snk_y = sh/2
-snake = [ 
-    [snk_y, snk_x],
-    [snk_y, snk_x-1],
-    [snk_y, snk_x-2],
-]
-
-food = [sh/2, sw/2]
-w.adddch(food[0], food[1], curses.ACS_PI)
-
-key = curses.KEY_RIGHT
+# game logic
+score = 0
 
 while True:
-    next_key
+    event = win.getch()
+
+    curses.endwin()
+    print(f"Final score = {score}")
